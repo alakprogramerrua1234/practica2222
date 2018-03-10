171fa7fc23d8de0ed5df0929d8940b4676f30ac2
alakprogramerrua1234 <alakrua@gmail.com>
2018-03-09 22:12:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/samirvega/practica2actividades/Actividad_principal.java
@@ -0,0 +1,15 @@
+package com.example.samirvega.practica2actividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_principal);
+
+        //hasta aqui llegue
+    }
+}
